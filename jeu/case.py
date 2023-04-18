70241f212d2fb620a7986d58a69fd18b634b1813
ClemPat <clement.patrizio@outlook.fr>
2023-04-18 14:07:55 +0200

    Add files via upload

---
+++ jeu/case.py
@@ -0,0 +1,13 @@
+
+
+class Case():
+    def __init__(cls, val, position):
+        cls.valeur = val
+        cls.position = position
+        # return super().__new__(cls, val)
+
+    def __repr__(self):
+        return f'{self.valeur}'
+
+    def __int__(self):
+        return self.valeur
---
+++ jeu/main.py
@@ -0,0 +1,5 @@
+import matrix
+
+
+if __name__ == '__main__':
+    matrice = matrix.Matrix()
