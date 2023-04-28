db4dea15da1c629ab6b7827a2d1825ddda144416
ClemPat <clement.patrizio@outlook.fr>
2023-04-28 18:17:20 +0200

    Delete window.py

--- window.py
+++
@@ -1,8 +0,0 @@
-
-
-class Window():
-    def __init__(self, cmd_caller):
-        self.cmd_caller = cmd_caller
-
-    def __repr__(self):
-        return f'fenêtre graphique pour la matrice {self.cmd_caller.matrix_caller}'
